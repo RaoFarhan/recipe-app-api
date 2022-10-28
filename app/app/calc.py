22b5fd47cda27340296e9705077a0702d321e8dd
Rao Farhan <farhan8418g@gmail.com>
2022-10-28 23:33:53 +0200

    Testing enviornment

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""" 
+Calculator Function
+"""
+
+def add(x, y):
+    """Add x and y and return result. """
+    return x + y
+
---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class ClacTests(SimpleTestCase):
+    """Test the calc module """
+    def test_add_numbers(self):
+        """Test adding numbers togeather"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 12)
