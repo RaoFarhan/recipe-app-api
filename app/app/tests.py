dd0f183c39edfb78df754a9259eebf48b1150dab
Rao Farhan <farhan8418g@gmail.com>
2022-10-28 23:54:24 +0200

    now

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,8 +7,11 @@ from app import calc
 
 class ClacTests(SimpleTestCase):
     """Test the calc module """
+   
     def test_add_numbers(self):
+
         """Test adding numbers togeather"""
+
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
