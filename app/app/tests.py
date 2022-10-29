8d645a2bd7f9c68ef37ce7e9d5f38f19fbcbaaf3
Rao Farhan <farhan8418g@gmail.com>
2022-10-29 10:02:04 +0200

    check

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,8 +5,10 @@ from  django.test import SimpleTestCase
 
 from app import calc
 
+
 class ClacTests():
     """Test the calc module"""
+
     
     def test_add_numbers(self):
         """Test adding numbers Together"""
