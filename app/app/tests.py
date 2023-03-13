6fbab9a971d00086f2e856f8c933755948542693
Farhan Yaqoob <farhan8418g@gmail.com>
2023-03-13 21:02:52 +0100

    Configuring database

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample Test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class ClacTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers Together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test Subtracting Number"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
