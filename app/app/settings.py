a60413857c019416bf055e2f79e58fa19c5bfc7b
Farhan Yaqoob <farhan8418g@gmail.com>
2023-03-14 14:42:10 +0100

    API Documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'drf_spectacular',
+    #'user',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
