c3bfb4997e4dc8f4d0a37d15e66aaf9af65c755e
Farhan Yaqoob <farhan8418g@gmail.com>
2023-03-14 14:57:44 +0100

    API Documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,13 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
+    #'rest_framework.authtoken',
     'core',
+    'rest_framework',
     'drf_spectacular',
     #'user',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +27,5 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-
-    ),
-    path('api/user/', include('user.urls')),
+    )
 ]
