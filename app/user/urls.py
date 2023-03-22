455274f3c543b18ea6559d136c2083e05d2a2def
Farhan Yaqoob <farhan8418g@gmail.com>
2023-03-22 13:08:51 +0100

    Api loading error

--- app/user/urls.py
+++ app/user/urls.py
@@ -3,7 +3,7 @@ URL mappings for the user API.
 """
 from django.urls import path
 
-from user import views
+from . import views
 
 
 app_name = 'user'
--- app/user/views.py
+++ app/user/views.py
@@ -5,10 +5,7 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import (
-    UserSerializer,
-    AuthTokenSerializer,
-)
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -25,8 +22,8 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
