d88107188f56d72b20313ee57d95e6694a3dc0c4
Farhan Yaqoob <farhan8418g@gmail.com>
2023-03-28 17:50:56 +0200

    Build Ingredient API

--- app/core/models.py
+++ app/core/models.py
@@ -5,8 +5,11 @@ Database models.
 
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, \
-    BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
 class UserManager(BaseUserManager):
@@ -56,7 +59,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
-    Ingredients = models.ManyToManyField('Ingredient')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
